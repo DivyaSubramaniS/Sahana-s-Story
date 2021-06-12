34a374d5615708be9aa64cd9717118bff6dd4016
Divya <divs.divya06@gmail.com>
2021-06-12 18:37:54 +0530

    ADDED STORY LINE

---
+++ Story.js
@@ -0,0 +1,3 @@
+**Story**
+    Once upon a time, there lived a rabbit in a forest. Rabbit had a friend called tortoise, He called his friend for a running race.
+His friend tortoise accepted for the race. Next day they conducted the race.     
